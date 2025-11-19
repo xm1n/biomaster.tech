4f738ec449e4a5ca745f630722f30fdabf88eb39
root <root@bitrix.bio-tualet.by>
2025-11-20 01:09:42 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?>
+
+Text here....
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
